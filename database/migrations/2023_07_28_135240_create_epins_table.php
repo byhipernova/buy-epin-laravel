623ff54b0544923916f164530fee3db62dfec2e5
Muhammet KÖKLÜ <105980019+byhipernova@users.noreply.github.com>
2023-07-29 01:27:57 +0300

    user_id added

--- database/migrations/2023_07_28_135240_create_epins_table.php
+++ database/migrations/2023_07_28_135240_create_epins_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('pin');
             $table->string('status');
+            $table->string('user_id')->nullable();
             $table->timestamps();
         });
     }
