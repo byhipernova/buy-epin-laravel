ce291af2e6455c79e6ccfdd03ede8559ca37af07
Muhammet KÖKLÜ <105980019+byhipernova@users.noreply.github.com>
2023-07-29 01:28:21 +0300

    epin controller

--- app/Http/Controllers/EpinController.php
+++ app/Http/Controllers/EpinController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Epin;
-use Illuminate\Http\Request;
 
 class EpinController extends Controller
 {
     function buyEpin()
     {
-        return Epin::all();
+        try {
+            $userid = rand(999999, 999999999).time();
+            $epin = Epin::where('status', 'unused')->limit(1)->update(['status' => 'used', 'user_id' => $userid]);
+
+            if ($epin) {
+                return Epin::where('user_id', $userid)->orderBy('id', 'desc')->first();
+            } else {
+                return response()->json([
+                    'message' => 'No epin available',
+                ], 404);
+            }
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => 'Error occurred while processing the request. message: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 }
