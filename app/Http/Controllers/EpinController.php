773c94ee021f5aa6421db9f1f746cb07b9362401
Muhammet KÖKLÜ <105980019+byhipernova@users.noreply.github.com>
2023-07-29 10:50:27 +0300

    Update EpinController.php

--- app/Http/Controllers/EpinController.php
+++ app/Http/Controllers/EpinController.php
@@ -6,23 +6,60 @@ use App\Models\Epin;
 
 class EpinController extends Controller
 {
+
+    /* Herhangi bir user istek attığında epin status değerini in process olarak ayarlıyoruz isteği atan user için user_id'yi unique oluşturduğum için user_id
+    kullandım fakat gerçek hayat senaryosunda process_id kullanılması daha iyi olacaktır error handling ile işlemlerde bir sorun çıkması durumunda epini
+    tekrar satışa sunmalıyız. Bu çözümü kullanmamdaki sebep mysqlin işlemleri sırayla yapması birden çok kullanıcı aynı anda epin isteğinde bulunsa bile
+    her kullanıcıya unique bi epin rezerve edilecek. Bu çözüm dışında lockForUpdate methodunu da gördüm fakat en mantıklı ve kolay çözüm bu bence */
     function buyEpin()
     {
         try {
-            $userid = rand(999999, 999999999).time();
-            $epin = Epin::where('status', 'unused')->limit(1)->update(['status' => 'used', 'user_id' => $userid]);
+            $userId = rand(999999, 999999999).time();
+            $update = Epin::where('status', 'unused')->limit(1)->update(['status' => 'in process', 'user_id' => $userId]);
 
-            if ($epin) {
-                return Epin::where('user_id', $userid)->orderBy('id', 'desc')->first();
+            if ($update) {
+
+                try {
+
+                    if ($this->userCanBuyEpin()) {
+                        Epin::where(['user_id' => $userId, 'status' => 'in process'])->update(['status' => 'used']);
+                        return Epin::where(['user_id' => $userId, 'status' => 'used'])->orderBy('id', 'desc')->first();
+                    } else {
+                        Epin::where(['user_id' => $userId, 'status' => 'in process'])->update(['status' => 'unused', 'user_id' => null]);
+                        return response()->json([
+                            'message' => 'User can not buy epin',
+                        ], 400);
+                    }
+
+                } catch (QueryException $e) {
+
+                    Epin::where(['user_id' => $userId, 'status' => 'in process'])->update(['status' => 'unused', 'user_id' => null]);
+                    return response()->json([
+                        'message' => 'Error occurred while processing the request. message: ' . $e->getMessage(),
+                    ], 500);
+
+                }
+
             } else {
+
                 return response()->json([
                     'message' => 'No epin available',
                 ], 404);
+
             }
         } catch (QueryException $e) {
+
             return response()->json([
                 'message' => 'Error occurred while processing the request. message: ' . $e->getMessage(),
             ], 500);
+
         }
     }
+
+    function userCanBuyEpin()
+    {
+        // user işlemleri
+        return true;
+    }
+
 }
